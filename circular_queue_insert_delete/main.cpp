cc97121813af30fbb129d87be9da30eaf141a13b
KarnageKnight <karnageknight@KarnageKnights-MacBook-Pro.local>
2015-01-30 09:42:09 +0530

    Initial Commit

---
+++ circular_queue_insert_delete/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  circular_queue_insert_delete
+//
+//  Created by KarnageKnight on 30/01/15.
+//  Copyright (c) 2015 KKCPP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
